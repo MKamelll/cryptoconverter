0fe40ed347c0acd3ada3e0c6f733ca32217652e2
Mina Kamel <minallkamel@gmail.com>
2025-01-31 15:13:40 +0200

    implement coin model

--- converter/models.py
+++ converter/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Coin(models.Model):
+    name: models.CharField = models.CharField(max_length=100)
+    price: models.CharField = models.CharField(max_length=100)
+    updated_at: models.DateTimeField = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Currency(name: {self.name}, price: {self.price}, updated_at: {self.updated_at})"
--- converter/views.py
+++ converter/views.py
@@ -1,8 +1,15 @@
+# https://api.binance.com/api/v3/ticker/price?symbol=SOLUSDT
+# https://api.binance.com/api/v3/exchangeInfo
+
+
 from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.shortcuts import render
-from . import api
+import requests as rq
+from .models import Coin
+from django.utils import timezone
 
-# Create your views here.
+BASE_QUERY_PRICE_URL = "https://api.binance.com/api/v3/ticker/price?symbol="
+BASE_QUERY_AGAINST_SYMBOL = "USDT"
 
 
 def convert(req: HttpRequest) -> HttpResponse:
@@ -12,7 +19,33 @@ def convert(req: HttpRequest) -> HttpResponse:
 def search(req: HttpRequest) -> JsonResponse:
     all_params = req.GET
     if 'coin' in all_params:
-        coin = all_params['coin']
-        data = api.query(coin)
-        return JsonResponse(data)
+        coin_param = all_params['coin'].lower()
+        coin = Coin.objects.filter(name=coin_param).first()
+        if coin:
+            duration = timezone.now() - coin.updated_at
+            if duration.seconds < 5 * 60:
+                return JsonResponse({
+                    'price': coin.price,
+                    'status': 200
+                })
+
+        res = rq.get(BASE_QUERY_PRICE_URL + coin_param.upper() +
+                     BASE_QUERY_AGAINST_SYMBOL)
+        if res.status_code == 200:
+            data = res.json()
+            coin = Coin.objects.create(
+                name=coin_param,
+                price=data['price'],
+                updated_at=timezone.now()
+            )
+            coin.save()
+            return JsonResponse({
+                'price': data['price'],
+                'status': 200
+            })
+        else:
+            return JsonResponse({
+                'msg': res.json()['msg'],
+                'status': 404
+            })
     return JsonResponse({'msg': "No coin was provided in query parameter", 'status': '404'})
